4605b32b6571134a74628f4d7e085fd33c8d67fd
JaniKacic <jk4117@student.uni-lj.si>
2020-11-19 18:01:58 +0100

    Added frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="blue-grey darken-4"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Enotes</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+<template >
+	<v-container>
+		<v-row>
+            <v-col cols="12">
+				<h2 class="headline font-weight-bold mb-3">
+					Here are your notes:
+				</h2>
+            </v-col>
+            <div v-for="note in notes" :key="note.title" class="ma-lg-1 ma-md-1">
+				<v-card
+                    elevation="1" 
+                    outlined
+                    max-width="500"
+                    
+                >
+					<v-img
+                        src="https://static8.depositphotos.com/1007173/1012/i/600/depositphotos_10129093-stock-photo-note-with-pin.jpg"
+                        height="100px"
+                    ></v-img>
+
+                    <v-card-title>
+                        {{ note.title }}
+                    </v-card-title>
+
+                    <v-card-subtitle>
+                        Reminder:
+                    </v-card-subtitle>
+
+                     <v-card-actions>
+                        <v-btn
+                            color="red lighten-1"
+                            text
+                        >
+                            Remove
+                        </v-btn>
+
+                        <v-spacer></v-spacer>
+
+                        <v-btn
+                            icon
+                            @click="note.show = !note.show"
+                        >
+                            <v-icon>{{ note.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
+
+                    <v-expand-transition>
+                        <div v-show="note.show">
+                            <v-divider></v-divider>
+                            <v-card-text>
+                                {{ note.text }}
+                            </v-card-text>
+                        </div>
+                    </v-expand-transition>
+				</v-card>
+            </div>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+	export default {
+		name: 'HelloWorld',
+
+		data: () => ({
+			notes: [
+				{
+                    title: 'Note1',
+                    text: 'Description of note1',
+                    show: false,
+				},
+				{
+                    title: 'Note2',
+                    text: 'Description of note2',
+                    show: false,
+				},
+				{
+                    title: 'Note3',
+                    text: 'Description of note3',
+                    show: false,
+				},
+			],
+            
+		}),
+	}
+</script>
