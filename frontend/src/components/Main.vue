5f5afa69fbac1fe57fd57bc592a91ec72c646df9
Timotej <20747466+Timic3@users.noreply.github.com>
2020-11-20 15:37:40 +0100

    Add ESLint for spaces and run format

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,135 +1,135 @@
 <template>
-	<v-container>
-		<v-row>
-            <v-col cols="12">
-                <v-btn
-                    color="green lighten-1"
-                    text
-                >
-                    Add note
-                </v-btn>
-                <v-btn
-                    color="green lighten-1"
-                    text
-                >
-                    Calendar
-                </v-btn>
-                <v-btn
-                    color="red lighten-1"
-                    text
-                >
-                    Clear
-                </v-btn>
-            </v-col>
-            <div v-for="note in notes" :key="note.title" class="ma-lg-1 ma-md-1" >
-				<v-card
-                    elevation="1" 
-                    outlined
-                    max-width="438"
-                >
-					<v-img
-                        src="https://static8.depositphotos.com/1007173/1012/i/600/depositphotos_10129093-stock-photo-note-with-pin.jpg"
-                        height="100px"
-                    ></v-img>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-btn
+          color="green lighten-1"
+          text
+        >
+          Add note
+        </v-btn>
+        <v-btn
+          color="green lighten-1"
+          text
+        >
+          Calendar
+        </v-btn>
+        <v-btn
+          color="red lighten-1"
+          text
+        >
+          Clear
+        </v-btn>
+      </v-col>
+      <div v-for="note in notes" :key="note.title" class="ma-lg-1 ma-md-1" >
+        <v-card
+          elevation="1" 
+          outlined
+          max-width="438"
+        >
+          <v-img
+            src="https://static8.depositphotos.com/1007173/1012/i/600/depositphotos_10129093-stock-photo-note-with-pin.jpg"
+            height="100px"
+          ></v-img>
 
-                    <v-card-title>
-                        {{ note.title }}
-                    </v-card-title>
+          <v-card-title>
+            {{ note.title }}
+          </v-card-title>
 
-                    <v-card-subtitle>
-                        Reminder:
-                    </v-card-subtitle>
+          <v-card-subtitle>
+            Reminder:
+          </v-card-subtitle>
 
-                     <v-card-actions>
-                        <v-btn
-                            color="red lighten-1"
-                            text
-                        >
-                            Remove
-                        </v-btn>
+          <v-card-actions>
+            <v-btn
+              color="red lighten-1"
+              text
+            >
+              Remove
+            </v-btn>
 
-                        <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-                        <v-btn
-                            icon
-                            @click="note.show = !note.show"
-                        >
-                            <v-icon>{{ note.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                        </v-btn>
-                    </v-card-actions>
+            <v-btn
+              icon
+              @click="note.show = !note.show"
+            >
+              <v-icon>{{ note.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+            </v-btn>
+          </v-card-actions>
 
-                    <v-expand-transition>
-                        <div v-show="note.show">
-                            <div  v-if="note.type=='Normal'">
-                                <v-divider></v-divider>
-                                <v-card-text>
-                                    {{ note.text }}
-                                </v-card-text>
-                            </div>
-                            <div  v-if="note.type=='TODO'">
-                                <v-divider></v-divider>
-                                <v-list flat subheader three-line>
-                                    <v-list-item-group
-                                        v-model="settings"
-                                        multiple
-                                        active-class=""
-                                        v-for="item in note.items" :key="item"
-                                    >
-                                    <v-list-item>
-                                        <template v-slot:default="{ active }">
-                                            <v-list-item-action>
-                                            <v-checkbox :input-value="active"></v-checkbox>
-                                            </v-list-item-action>
+          <v-expand-transition>
+            <div v-show="note.show">
+              <div  v-if="note.type=='Normal'">
+                <v-divider></v-divider>
+                <v-card-text>
+                  {{ note.text }}
+                </v-card-text>
+              </div>
+              <div  v-if="note.type=='TODO'">
+                <v-divider></v-divider>
+                <v-list flat subheader three-line>
+                  <v-list-item-group
+                    v-model="settings"
+                    multiple
+                    active-class=""
+                    v-for="item in note.items" :key="item"
+                  >
+                    <v-list-item>
+                      <template v-slot:default="{ active }">
+                        <v-list-item-action>
+                          <v-checkbox :input-value="active"></v-checkbox>
+                        </v-list-item-action>
 
-                                            <v-list-item-content>
-                                                <v-list-item-title>{{ item[0] }}</v-list-item-title>
-                                                <v-list-item-subtitle>{{ item[1] }}</v-list-item-subtitle>
-                                            </v-list-item-content>
-                                        </template>
-                                    </v-list-item>
-                                    </v-list-item-group>
-                                </v-list>
-                            </div>
-                        </div>
-                    </v-expand-transition>
-				</v-card>
+                        <v-list-item-content>
+                          <v-list-item-title>{{ item[0] }}</v-list-item-title>
+                          <v-list-item-subtitle>{{ item[1] }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                      </template>
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </div>
             </div>
-		</v-row>
-	</v-container>
+          </v-expand-transition>
+        </v-card>
+      </div>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-	export default {
-		name: 'Main',
+export default {
+  name: 'Main',
 
-		data: () => ({
-			notes: [
-				{
-                    title: 'Note1',
-                    type: 'Normal',
-                    text: 'Description of note1',
-                    show: false,
-				},
-				{
-                    title: 'Note2',
-                    type: 'Normal',
-                    text: 'Description of note2',
-                    show: false,
-                },
-                {
-                    title: 'TODO Note1',
-                    type: 'TODO',
-                    items: [["Clean room","This is description of Clean room"], ["Wash dishes", "This is description of Wash dishes"], ["Do your homework", "This is description of Do your homework"]],
-                    show: false,
-				},
-				{
-                    title: 'Note3',
-                    type: 'Normal',
-                    text: 'Description of note3',
-                    show: false,
-				},
-			],
+  data: () => ({
+    notes: [
+      {
+        title: 'Note1',
+        type: 'Normal',
+        text: 'Description of note1',
+        show: false,
+      },
+      {
+        title: 'Note2',
+        type: 'Normal',
+        text: 'Description of note2',
+        show: false,
+      },
+      {
+        title: 'TODO Note1',
+        type: 'TODO',
+        items: [["Clean room","This is description of Clean room"], ["Wash dishes", "This is description of Wash dishes"], ["Do your homework", "This is description of Do your homework"]],
+        show: false,
+      },
+      {
+        title: 'Note3',
+        type: 'Normal',
+        text: 'Description of note3',
+        show: false,
+      },
+    ],
             
-		}),
-	}
+  }),
+}
 </script>
--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,187 +1,187 @@
 <template>
-    <div>
-        <v-card class="mx-auto mt-6 pa-6" max-width="500" v-if="login == true">
-            <v-form
-                ref="form"
-                v-model="valid"
-                lazy-validation
-            >
-                <v-text-field
-                v-model="username"
-                :counter="10"
-                :rules="nameRules"
-                label="Userame"
-                required
-                ></v-text-field>
+  <div>
+    <v-card class="mx-auto mt-6 pa-6" max-width="500" v-if="login == true">
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="username"
+          :counter="10"
+          :rules="nameRules"
+          label="Userame"
+          required
+        ></v-text-field>
 
-                <v-text-field
-                    v-model="password"
-                    :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                    :rules="[rules.required, rules.min]"
-                    :type="show ? 'text' : 'password'"
-                    name="input-10-1"
-                    label="Password"
-                    hint="At least 8 characters"
-                    counter
-                    @click:append="show = !show"
-                    class="mb-3"
-                ></v-text-field>
+        <v-text-field
+          v-model="password"
+          :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]"
+          :type="show ? 'text' : 'password'"
+          name="input-10-1"
+          label="Password"
+          hint="At least 8 characters"
+          counter
+          @click:append="show = !show"
+          class="mb-3"
+        ></v-text-field>
 
-                <v-btn
-                :disabled="!valid"
-                color="success"
-                class="mr-4"
-                @click="validate"
-                >
-                Login
-                </v-btn>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Login
+        </v-btn>
 
-                <v-btn
-                color="success"
-                class="mr-4"
-                @click="change"
-                >
-                Register
-                </v-btn>
+        <v-btn
+          color="success"
+          class="mr-4"
+          @click="change"
+        >
+          Register
+        </v-btn>
 
-                <v-btn
-                color="error"
-                class="mr-4"
-                @click="reset"
-                >
-                Reset Form
-                </v-btn>
-            </v-form>
-        </v-card>
-        <v-card class="mx-auto mt-6 pa-6" max-width="500" v-else>
-            <v-form
-                ref="form"
-                v-model="valid"
-                lazy-validation
-            >
-                <v-text-field
-                v-model="usernameReg"
-                :counter="10"
-                :rules="nameRules"
-                label="Userame"
-                required
-                ></v-text-field>
+        <v-btn
+          color="error"
+          class="mr-4"
+          @click="reset"
+        >
+          Reset Form
+        </v-btn>
+      </v-form>
+    </v-card>
+    <v-card class="mx-auto mt-6 pa-6" max-width="500" v-else>
+      <v-form
+        ref="form"
+        v-model="valid"
+        lazy-validation
+      >
+        <v-text-field
+          v-model="usernameReg"
+          :counter="10"
+          :rules="nameRules"
+          label="Userame"
+          required
+        ></v-text-field>
 
-                <v-text-field
-                v-model="email"
-                :rules="emailRules"
-                label="E-mail"
-                required
-                ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
 
-                <v-text-field
-                    v-model="passwordReg"
-                    :rules="[rules.required, rules.min]"
-                    :type="show ? 'text' : 'password'"
-                    name="input-10-1"
-                    label="Password"
-                    hint="At least 8 characters"
-                    counter
-                    class="mb-3"
-                ></v-text-field>
+        <v-text-field
+          v-model="passwordReg"
+          :rules="[rules.required, rules.min]"
+          :type="show ? 'text' : 'password'"
+          name="input-10-1"
+          label="Password"
+          hint="At least 8 characters"
+          counter
+          class="mb-3"
+        ></v-text-field>
 
-                <v-text-field
-                    v-model="confirmpassword"
-                    :rules="[rules.required, rules.min, passwordConfirmationRule]"
-                    :type="show ? 'text' : 'password'"
-                    name="input-10-2"
-                    label="Confirm password"
-                    hint="At least 8 characters"
-                    counter
-                    class="mb-3"
-                ></v-text-field>
+        <v-text-field
+          v-model="confirmpassword"
+          :rules="[rules.required, rules.min, passwordConfirmationRule]"
+          :type="show ? 'text' : 'password'"
+          name="input-10-2"
+          label="Confirm password"
+          hint="At least 8 characters"
+          counter
+          class="mb-3"
+        ></v-text-field>
 
-                <v-checkbox
-                v-model="checkbox"
-                :rules="[v => !!v || 'You must agree to continue!']"
-                label="Do you agree to our terms?"
-                required
-                class="mb-3"
-                ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[v => !!v || 'You must agree to continue!']"
+          label="Do you agree to our terms?"
+          required
+          class="mb-3"
+        ></v-checkbox>
 
-                <v-btn
-                :disabled="!valid"
-                color="success"
-                class="mr-4"
-                @click="validate"
-                >
-                Confirm
-                </v-btn>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="validate"
+        >
+          Confirm
+        </v-btn>
 
-                <v-btn
-                color="success"
-                class="mr-4"
-                @click="change"
-                >
-                Back
-                </v-btn>
+        <v-btn
+          color="success"
+          class="mr-4"
+          @click="change"
+        >
+          Back
+        </v-btn>
 
-                <v-btn
-                color="error"
-                class="mr-4"
-                @click="reset"
-                >
-                Reset Form
-                </v-btn>
-            </v-form>
-        </v-card>
-    </div>
+        <v-btn
+          color="error"
+          class="mr-4"
+          @click="reset"
+        >
+          Reset Form
+        </v-btn>
+      </v-form>
+    </v-card>
+  </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-        login: true,
-        valid: true,
-        username: '',
-        password: '',
-        nameRules: [
-            v => !!v || 'Name is required',
-            v => (v && v.length <= 10) || 'Name must be less than 10 characters',
-        ],
+export default {
+  data: () => ({
+    login: true,
+    valid: true,
+    username: '',
+    password: '',
+    nameRules: [
+      v => !!v || 'Name is required',
+      v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+    ],
 
-        email: '',
-        emailRules: [
-            v => !!v || 'E-mail is required',
-            v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-        ],
-        select: null,
-        checkbox: false,
+    email: '',
+    emailRules: [
+      v => !!v || 'E-mail is required',
+      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+    ],
+    select: null,
+    checkbox: false,
 
-        show: false,
-        usernameReg: '',
-        passwordReg: '',
-        confirmpassword: '',
-        rules: {
-          required: value => !!value || 'Required.',
-          min: v => v.length >= 8 || 'Min 8 characters',
-        },
-    }),
-
-    computed: {
-        passwordConfirmationRule() {
-            return () => (this.password === this.confirmpassword) || 'Password must match'
-        }
+    show: false,
+    usernameReg: '',
+    passwordReg: '',
+    confirmpassword: '',
+    rules: {
+      required: value => !!value || 'Required.',
+      min: v => v.length >= 8 || 'Min 8 characters',
     },
+  }),
 
-    methods: {
-        validate () {
-            this.$refs.form.validate()
-        },
-        reset () {
-            this.$refs.form.reset()
-        },
-        resetValidation () {
-            this.$refs.form.resetValidation()
-        },
-        change (){
-            this.login = !this.login;
-        },
+  computed: {
+    passwordConfirmationRule() {
+      return () => (this.password === this.confirmpassword) || 'Password must match'
+    }
+  },
+
+  methods: {
+    validate () {
+      this.$refs.form.validate()
     },
-  }
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    },
+    change (){
+      this.login = !this.login;
+    },
+  },
+}
 </script>
