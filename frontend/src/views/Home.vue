ffb4462b654c3661c06070783b5567c968825c6d
JaniKacic <jk4117@student.uni-lj.si>
2020-12-29 14:13:19 +0100

    added dragable notes and ui cleanup

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -31,85 +31,84 @@
           Clear
         </v-btn>-->
       </v-col>
-      <div v-for="note in notes" :key="note.title" class="ma-lg-1 ma-md-1 ma-sm-3 ma-xs-5" >
-        <v-card
-          elevation="1" 
-          outlined
-          max-width="438"
-          v-if="note.sh"
-        >
-          <v-img
-            src="https://static8.depositphotos.com/1007173/1012/i/600/depositphotos_10129093-stock-photo-note-with-pin.jpg"
-            height="100px"
-          ></v-img>
+      <div v-for="note in notes" :key="note.id" class="ma-lg-1 ma-md-1 ma-sm-3 ma-xs-5" >
+        <DraggableDiv v-if="note.sh">
+          <template slot="header">
+            <v-img
+              src="https://static8.depositphotos.com/1007173/1012/i/600/depositphotos_10129093-stock-photo-note-with-pin.jpg"
+              height="100px"
+            ></v-img>
+          </template>
+          <template slot="main" >
+            <v-card-title>
+              {{ note.title }}
+            </v-card-title>
 
-          <v-card-title>
-            {{ note.title }}
-          </v-card-title>
+            <v-card-subtitle>
+              Reminder:
+            </v-card-subtitle>
 
-          <v-card-subtitle>
-            Reminder:
-          </v-card-subtitle>
+            <v-card-actions>
+              <v-btn
+                color="red lighten-1"
+                text
+                @click="removeNote(note.id)"
+              >
+                Remove
+              </v-btn>
 
-          <v-card-actions>
-            <v-btn
-              color="red lighten-1"
-              text
-              @click="removeNote(note.id)"
-            >
-              Remove
-            </v-btn>
+              <v-spacer></v-spacer>
 
-            <v-spacer></v-spacer>
+              <v-btn
+                icon
+                @click="note.show = !note.show"
+              >
+                <v-icon>{{ note.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
 
-            <v-btn
-              icon
-              @click="note.show = !note.show"
-            >
-              <v-icon>{{ note.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-            </v-btn>
-          </v-card-actions>
+            <v-expand-transition>
+              <div v-show="note.show">
+                <div  v-if="note.type=='Normal'">
+                  <v-divider></v-divider>
+                  <v-card-text>
+                    {{ note.text }}
+                  </v-card-text>
+                </div>
+                <div  v-if="note.type=='Todo'">
+                  <v-divider></v-divider>
+                  <v-list flat subheader three-line>
+                    <v-list-item-group
+                      multiple
+                      active-class=""
+                      v-for="item in note.items" :key="item[0]"
+                    >
+                      <v-list-item>
+                        <template v-slot:default="{ active }">
+                          <v-list-item-action>
+                            <v-checkbox :input-value="active" v-model="item.checked" @click="itemCheck(item)"></v-checkbox>
+                          </v-list-item-action>
 
-          <v-expand-transition>
-            <div v-show="note.show">
-              <div  v-if="note.type=='Normal'">
-                <v-divider></v-divider>
-                <v-card-text>
-                  {{ note.text }}
-                </v-card-text>
+                          <v-list-item-content>
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                            <!--<v-list-item-subtitle>{{ item.text }}</v-list-item-subtitle>-->
+                          </v-list-item-content>
+                        </template>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </div>
               </div>
-              <div  v-if="note.type=='Todo'">
-                <v-divider></v-divider>
-                <v-list flat subheader three-line>
-                  <v-list-item-group
-                    multiple
-                    active-class=""
-                    v-for="item in note.items" :key="item[0]"
-                  >
-                    <v-list-item>
-                      <template v-slot:default="{ active }">
-                        <v-list-item-action>
-                          <v-checkbox :input-value="active" v-model="item.checked" @click="itemCheck(item)"></v-checkbox>
-                        </v-list-item-action>
-
-                        <v-list-item-content>
-                          <v-list-item-title>{{ item.title }}</v-list-item-title>
-                          <!--<v-list-item-subtitle>{{ item.text }}</v-list-item-subtitle>-->
-                        </v-list-item-content>
-                      </template>
-                    </v-list-item>
-                  </v-list-item-group>
-                </v-list>
-              </div>
-            </div>
-          </v-expand-transition>
-        </v-card>
+            </v-expand-transition>
+          </template>
+          <template slot="footer"></template>
+        </DraggableDiv>
       </div>
     </v-row>
     <v-overlay
       v-if="add"
     >
-      <v-card class="mx-auto mt-6 pa-6" max-width="800" light>
+      <v-card class="mx-auto mt-6 pa-6" max-width="800" light elevation="1" >
         <v-text-field
           v-model="title"
           label="Title"
@@ -118,12 +117,63 @@
           v-model="text"
           name="text"
           label="Text"
+          rows="1"
+          auto-grow
         ></v-textarea>
-        <v-date-picker
-          v-model="date"
-          class="mt-4"
+        <v-menu
+          v-model="menu1"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
           v-if="reminder"
-        ></v-date-picker>
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="date"
+              label="Select date of reminder"
+              prepend-icon="mdi-calendar"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-date-picker
+            v-model="date"
+            @input="datepicker = false"
+          ></v-date-picker>
+        </v-menu>
+        <v-menu
+          ref="menu"
+          v-model="menu2"
+          :close-on-content-click="false"
+          :nudge-right="40"
+          :return-value.sync="time"
+          transition="scale-transition"
+          offset-y
+          max-width="290px"
+          min-width="290px"
+          v-if="reminder"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+              v-model="time"
+              label="Picker in menu"
+              prepend-icon="mdi-clock-time-four-outline"
+              readonly
+              v-bind="attrs"
+              v-on="on"
+            ></v-text-field>
+          </template>
+          <v-time-picker
+            v-if="menu2"
+            v-model="time"
+            full-width
+            @click:minute="$refs.menu.save(time)"
+            format="24hr"
+          ></v-time-picker>
+        </v-menu>
         <v-text-field
           v-model="todo"
           label="Items (ex. item1, item2)"
@@ -181,6 +231,7 @@
 
 <script>
 import axios from 'axios';
+import DraggableDiv from './DraggableDiv'
 
 export default {
   name: 'Home',
@@ -189,12 +240,22 @@ export default {
     notes: [],
     add: false,
     reminder: false,
+    datepicker: false,
+    timepicker: false,
     title: '',
     text: '',
     todo: '',
-    date: '',
+    date: new Date().toISOString().substr(0, 10),
     type: '',
+    time: null,
+    menu1: false,
+    menu2: false,
+    modal2: false,
   }),
+
+  components: {
+    DraggableDiv
+  },
 
   computed: {
     user() {
