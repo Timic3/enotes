880623c709e4522702e74531deeddac0e44e2dfe
Timotej <20747466+Timic3@users.noreply.github.com>
2021-01-11 15:02:24 +0100

    Add initial CI

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [{
+    name: 'ENotes',
+    script: 'src/index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy: {
+    production: {
+      key: 'enotes-key',
+      user: process.env.SSH_USER,
+      host: process.env.SSH_HOST,
+      ref: 'origin/master',
+      repo: 'git@github.com:Timic3/enotes.git',
+      path: process.env.SSH_PATH,
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.cjs --env production'
+    }
+  }
+};
