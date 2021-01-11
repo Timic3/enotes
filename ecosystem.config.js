57fe37ad231523e55e59d0cfa975f251d64d072e
Timotej <20747466+Timic3@users.noreply.github.com>
2021-01-11 18:32:19 +0100

    Use backend path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'ENotes',
     interpreter: 'babel-node',
-    script: 'src/index.js',
+    script: 'backend/src/index.js',
     instances: 1,
     autorestart: true,
     watch: false,
