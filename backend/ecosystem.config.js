213c26b74ea8ba6f25eb7654980b6be5f5b6241f
Timotej <20747466+Timic3@users.noreply.github.com>
2021-01-11 18:28:30 +0100

    Put ecosystem inside backend as well

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [{
+    name: 'ENotes',
+    interpreter: 'babel-node',
+    script: 'src/index.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy: {
+    production: {
+      key: 'enotes-key',
+      user: process.env.SSH_USER,
+      host: process.env.SSH_HOST,
+      ref: 'origin/feature/ci',
+      repo: 'git@github.com:Timic3/enotes.git',
+      path: process.env.SSH_PATH,
+      'post-deploy': 'cd ./backend && npm install && pm2 reload ecosystem.config.js --env production'
+    },
+  }
+};
