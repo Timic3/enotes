6f6b7461fd6db22a163322893d39a383ae3475ef
Timotej <20747466+Timic3@users.noreply.github.com>
2021-01-12 22:26:55 +0100

    Generate docs

---
+++ backend/docs/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "@enotes/backend",
+  "version": "1.0.0",
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-01-12T21:26:40.805Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
